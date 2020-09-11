486883e02e9a0120bae84b4e717f4807fc0b5c74
MD. AL-AMIN <alaminhossen899@gmail.com>
2020-09-11 09:46:10 +0600

    component create and chat html template use

---
+++ resources/js/components/ChatApp.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="container clearfix">
+        <div class="people-list" style="min-height: 600px; overflow-y: scroll; height:500px;" >
+            <input type="text" placeholder="search" />
+            <i class="fa fa-search"></i>
+            <ul>
+                <li class="clearfix">
+                    <div class="about">
+                        <div class="name">Vincent Porter</div>
+                        <div class="status">
+                            <i class="fa fa-circle online"></i> online
+                        </div>
+                    </div>
+                </li>
+
+                <li class="clearfix">
+                    <div class="about">
+                        <div class="name">Aiden Chavez</div>
+                        <div class="status">
+                            <i class="fa fa-circle offline"></i> left 7 mins ago
+                        </div>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+
+        <div class="chat">
+            <div class="chat-header clearfix">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_01_green.jpg" alt="avatar" />
+
+                <div class="chat-about">
+                    <div class="chat-with">Chat with Vincent Porter</div>
+                    <div class="chat-num-messages">already 1 902 messages</div>
+                </div>
+                <i class="fa fa-star"></i>
+            </div> <!-- end chat-header -->
+
+            <div class="chat-history" style="overflow-y: scroll; height:300px;">
+                <ul>
+                    <li>
+                        <div class="message-data">
+                            <span class="message-data-name"><i class="fa fa-circle online"></i> Vincent</span>
+                            <span class="message-data-time">10:12 AM, Today</span>
+                        </div>
+                        <div class="message my-message">
+                            Are we meeting today? Project has been already finished and I have results to show you.
+                        </div>
+                    </li>
+
+                    <li class="clearfix">
+                        <div class="message-data align-right">
+                            <span class="message-data-time" >10:14 AM, Today</span> &nbsp; &nbsp;
+                            <span class="message-data-name" >Olia</span> <i class="fa fa-circle me"></i>
+
+                        </div>
+                        <div class="message other-message" style="float:right">
+                            Well I am not sure. The rest of the team is not here yet. Maybe in an hour or so? Have you faced any problems at the last phase of the project?
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="message-data">
+                            <span class="message-data-name"><i class="fa fa-circle online"></i> Vincent</span>
+                            <span class="message-data-time">10:20 AM, Today</span>
+                        </div>
+                        <div class="message my-message">
+                            Actually everything was fine. I'm very excited to show this to our team.
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="message-data">
+                            <span class="message-data-name"><i class="fa fa-circle online"></i> Vincent</span>
+                            <span class="message-data-time">10:31 AM, Today</span>
+                        </div>
+                        <i class="fa fa-circle online"></i>
+                        <i class="fa fa-circle online" style="color: #AED2A6"></i>
+                        <i class="fa fa-circle online" style="color:#DAE9DA"></i>
+                    </li>
+
+                </ul>
+
+            </div> <!-- end chat-history -->
+
+            <div class="chat-message clearfix">
+                <textarea name="chat-message-to-send" placeholder ="Type your message" rows="3"></textarea>
+
+                <i class="fa fa-file-o"></i> &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-file-image-o"></i>
+
+                <button>Send</button>
+
+            </div> <!-- end chat-message -->
+
+        </div> <!-- end chat -->
+
+    </div> <!-- end container -->
+</template>
+
+<script>
+    export default {
+        name: "ChatApp"
+    }
+</script>
+
+<style scoped>
+
+</style>
